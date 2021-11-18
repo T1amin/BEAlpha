6bd7b16ccaadd7289ef9d914c2bc09a03666e387
v1 <v.medicin@gmail.com>
2021-11-18 15:50:30 +0500

    TODO Image upload

--- src/frontendMain/kotlin/com/t1/BEAlpha/Admin.kt
+++ src/frontendMain/kotlin/com/t1/BEAlpha/Admin.kt
@@ -3,6 +3,13 @@ package com.t1.BEAlpha
 import com.t1.BEAlpha.Model.types
 import io.kvision.core.*
 import io.kvision.data.dataContainer
+import io.kvision.form.FormPanel
+import io.kvision.form.formPanel
+import io.kvision.form.spinner.Spinner
+import io.kvision.form.text.DataType
+import io.kvision.form.text.Text
+import io.kvision.form.time.DateTime
+import io.kvision.form.upload.Upload
 import io.kvision.html.*
 import io.kvision.modal.Confirm
 import io.kvision.modal.Modal
@@ -39,31 +46,41 @@ object Admin: SimplePanel() {
                     className = "modal fade bottom",
                     animation = true,
                 ){
-                    div(
-                        className = "modal-dialog modal-frame modal-bottom",
-                    ){
-                        maxWidth = 450.px
-                        minWidth = 180.px
-                        margin = 5.px
-                        flexShrink = 1
-                        div(
-                            className = "modal-body py-1"
-                        ){
-                            image(
-                                className = "img-fluid",
-                                alt = card.img,
-                                src = card.img
-                            )
-                        }
-                        div(className = "mb-0"){
-                            h5(className = "card-title", content = card.title)
-                            p(className = "card-text", content = card.price.toString() + " руб.")
-                            p(className = "card-text", content = card.description)
-                            p(className = "card-text", content = card.category)
-                            p(className = "card-text", content = "#" + card.tags)
-                            footer(className = "blockquote-footer", content = card.createdAt.toString())
-                        }
+                    formPanel<Card> {
+                        add(Card::title, Text(label = "Название").apply { maxlength = 255 })
+                        add(Card::price, Spinner(label = "Цена"))
+                        add(Card::category, Text(label = "Категория").apply { maxlength = 255 })
+                        add(Card::tags, Text(label = "Теги").apply { maxlength = 255 })
+                        add(Card::description, Text(label = "Описание"))
+                        add(Card::createdAt, DateTime(label = "Дата"))
+                        add(Card::img, Upload("/", multiple = false, label = "Upload files (images only)"){
+                            explorerTheme = true
+                            dropZoneEnabled = true
+                            allowedFileTypes = setOf("image")
+                            TODO()
+                        })
+                        button("Загрузить", type = ButtonType.SUBMIT)
                     }
+
+//                        div(
+//                            className = "modal-body py-1"
+//                        ){
+//                            image(
+//                                className = "img-fluid",
+//                                alt = card.img,
+//                                src = card.img
+//                            )
+//                        }
+//                        div(className = "mb-0"){
+//                            header(className = "card-text", content = card.id.toString())
+//                            h5(className = "card-title", content = card.title)
+//                            p(className = "card-text", content = card.price.toString() + " руб.")
+//                            p(className = "card-text", content = card.description)
+//                            p(className = "card-text", content = card.category)
+//                            p(className = "card-text", content = "#" + card.tags)
+//                            footer(className = "blockquote-footer", content = card.createdAt.toString())
+//                        }
+
                 }
                 add(position = index, Div(className = "card", ){
                     maxWidth = 450.px
